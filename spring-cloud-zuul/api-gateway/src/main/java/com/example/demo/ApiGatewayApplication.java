383295b2b146704caefd6e0331aa11ee9f26d93e
gaoshuai <269182979@qq.com>
2019-09-16 16:19:56 +0800

    spring-cloud-zuul
    服务网关zuul的初级使用

---
+++ spring-cloud-zuul/api-gateway/src/main/java/com/example/demo/ApiGatewayApplication.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+//@@EnableZuulProxy注解开启zuul的功能
+@EnableZuulProxy
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
---
+++ spring-cloud-zuul/eureka-client-api2/src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+	
+    @RequestMapping("/hello")
+    public String index(@RequestParam String name) {
+        return "hello "+name+"，this is first messge";
+    }
+    
+    @RequestMapping("/test")
+    public String index2(@RequestParam String name) {
+        return "hello "+name+"，this is test messge";
+    }
+}
